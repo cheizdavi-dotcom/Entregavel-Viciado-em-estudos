021cce9f93a7a3701919facceda8bd3c9e3f22f8
Davi Cheiz <davicheizz9@gmail.com>
2025-11-01 17:48:16 +0000

    erro 404 ainda

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -10,7 +10,7 @@ export default function AppLayout({
 }) {
   return (
     <ProgressProvider>
-        <AppShell>{children}</AppShell>
+      <AppShell>{children}</AppShell>
     </ProgressProvider>
   );
 }
