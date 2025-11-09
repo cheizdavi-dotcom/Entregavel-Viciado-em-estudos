dee576b8f2d4673188b024f7194d027459d868fc
Davi Cheiz <davicheizz9@gmail.com>
2025-11-09 22:25:22 +0000

    agora o link para aula 2 https://youtu.be/2odfnEAKNEU

--- src/lib/seed.ts
+++ src/lib/seed.ts
@@ -22,7 +22,7 @@ export const modules = [
     id: '3',
     title: 'Módulo 3: A Ação',
     subtitle: 'Técnicas de Produtividade',
-    coverUrl: 'https://imgur.com/rLKBy1M.png',
+    coverUrl: 'https://i.imgur.com/rLKBy1M.png',
     description: 'Descubra técnicas práticas e eficazes para iniciar e concluir tarefas, mesmo quando a motivação está baixa.',
     order: 3,
     summaryPdfUrl: 'https://drive.google.com/drive/folders/13_j9_y33umMWvrzfSvpfGWI0m1Yu2zWh?usp=drive_link',
@@ -80,7 +80,7 @@ export const lessons = [
 
   // Módulo 4
   { id: '401', moduleId: '4', title: 'Aula 1: Como hábitos funcionam', youtubeId: 'kSJVekvjViQ', order: 1, durationSec: 330, summaryPdfUrl: '' },
-  { id: '402', moduleId: '4', title: 'Aula 2: Empilhamento de hábitos', youtubeId: 'fzeHAfhp39Y', order: 2, durationSec: 290, summaryPdfUrl: '' },
+  { id: '402', moduleId: '4', title: 'Aula 2: Empilhamento de hábitos', youtubeId: '2odfnEAKNEU', order: 2, durationSec: 290, summaryPdfUrl: '' },
   { id: '403', moduleId: '4', title: 'Aula 3: O diário de progresso', youtubeId: 'KzZy35_k_wI', order: 3, durationSec: 310, summaryPdfUrl: '' },
   { id: '404', moduleId: '4', title: 'Aula 4: Lidando com falhas', youtubeId: 'X7gh_A3locY', order: 4, durationSec: 340, summaryPdfUrl: '' },
   { id: '405', moduleId: '4', title: 'Aula 5: A identidade de quem faz', youtubeId: 'PZ7lDrwYdZc', order: 5, durationSec: 300, summaryPdfUrl: '' },
